f1d8a2a426c62bf9dd4483c0a65e268367d2e99b
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-06-05 12:09:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ffhacka-client',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
