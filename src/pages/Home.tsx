7bbb189c03d7a19e83f929289ddbdd34f4c5fe6e
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-06-05 16:14:58 -0300

    improvement: started Login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
@@ -11,12 +16,6 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
       </IonContent>
     </IonPage>
   );
